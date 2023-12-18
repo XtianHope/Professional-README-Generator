a4ec14c79d8b25a93be40b2cc9958ebe3cad0d5c
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2023-12-17 23:00:04 -0500

    wrote function to write README fiel

--- index.js
+++ index.js
@@ -70,8 +70,14 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err)
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
