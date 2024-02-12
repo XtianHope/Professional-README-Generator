924711eedf056934c5696212ce48f3a4938a54e3
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-02-11 21:17:45 -0500

    made changes to index.js, was receiving error when trying to run node. Error fixed.

--- index.js
+++ index.js
@@ -16,52 +16,52 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'Please provide a description of your project:'
-    }
+    },
     // Installation
     {
         type: 'input',
         name: 'installation',
         message: 'Please describe the installation process:'
-    }
+    },
     // Usage
     {
         type: 'input',
         name: 'usage',
         message: 'Please describe how your project is intended to be used:'
-    }
+    },
     // Contribution
     {
         type: 'input',
         name: 'contribution',
         message: 'Please name the contributors of this project:'
-    }
+    },
     // Test Instructions
     {
         type: 'input',
         name: 'instructions',
         message: 'What are the testing protocols/instructions:'
-    }
+    },
     // License
     {
         type: 'list',
         name: 'license',
         message: 'Choose a license for your application:',
         choices: [
-            'Apache 2.0',
-            'GNU GPLv3',
-            'GNU LGPLv3',
+            'Apache-2.0',
+            'GNU-GPLv3',
+            'GNU-LGPLv3',
             'MIT',
-            'Boost Software 1.0',
+            'Boost-Software-1.0',
             'Mozilla',
             'ISC',
         ]
-    }
+    },
     // GitHub
     {
         type: 'input',
         name: 'github',
         message: 'What is your GitHub username?'
-    }
+    },
     // Email Address
     {
         type: 'input',
@@ -80,16 +80,11 @@ function writeToFile(fileName, data) {
     });
 }
 
-// Function to get License Badge URL
-function getLicense(license) {
-
-}
-
+  
 // Function to initialize app
 function init() {
-    inquirer.createPromptModule(questions).then((data) => {
+    inquirer.prompt(questions).then((data) => {
         console.log(JSON.stringify(data, null, " "));
-        data.licenseBadge = getLicense(data.license);
         writeToFile("./example/readme.md", data);
     });
 }
